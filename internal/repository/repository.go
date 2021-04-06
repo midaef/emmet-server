277647002d6353a7a04fc0ee6eedda786d4ef3fc
midaef <danil.present@mail.ru>
2021-04-07 02:57:45 +0800

    update(repository)
    * Add method(is exist by email)
    * Add method(get user role by login and password)
    * Update repository and add 2 methods to interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,11 +1,23 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"context"
+	"github.com/midaef/emmet-server/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type AuthRepository interface {
+	IsExistByEmail(ctx context.Context, login string) bool
+	GetUserRoleByLoginAndPassword(ctx context.Context, user *models.User) (string, error)
+}
 
 type Repositories struct {
-
+	AuthRepository AuthRepository
 }
 
 func NewRepositories(db *sqlx.DB) *Repositories {
-	return &Repositories{}
+	return &Repositories{
+		AuthRepository: NewAuthRepository(db),
+	}
 }
