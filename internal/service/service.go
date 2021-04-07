9b6b18266f8af35618781ebc8d59d159ad83b703
midaef <danil.present@mail.ru>
2021-04-07 22:08:40 +0800

    update(user service)
    * Add method for create user by access token
    * Update service(add user service interface)

--- internal/service/service.go
+++ internal/service/service.go
@@ -13,7 +13,7 @@ type AuthService interface {
 }
 
 type UserService interface {
-
+	CreateUserByAccessToken(ctx context.Context, req *api.CreateUserByAccessTokenRequest) (*api.CreateUserResponseByAccessToken, error)
 }
 
 type Services struct {
@@ -28,9 +28,8 @@ type Dependencies struct {
 }
 
 func NewServices(deps *Dependencies) *Services {
-
 	authService := NewAuthService(deps.Hasher, deps.JWTManager, deps.Repository.AuthRepository, deps.Repository.TokenRepository)
-	userService := NewUserService(deps.Hasher, deps.JWTManager, deps.Repository.UserRepository)
+	userService := NewUserService(deps.Hasher, deps.JWTManager, deps.Repository.UserRepository, deps.Repository.AuthRepository)
 
 	return &Services{
 		AuthService: authService,
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,20 +1,65 @@
 package service
 
 import (
+	"context"
+	"github.com/midaef/emmet-server/internal/models"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"time"
+
+	"github.com/midaef/emmet-server/internal/api"
 	"github.com/midaef/emmet-server/internal/repository"
 	"github.com/midaef/emmet-server/pkg/helpers"
 )
 
 type User struct {
-	hasher          *helpers.Md5
-	tokenManager    *helpers.JWT
-	userRepository  repository.UserRepository
+	hasher         *helpers.Md5
+	tokenManager   *helpers.JWT
+	userRepository repository.UserRepository
+	authRepository repository.AuthRepository
 }
 
-func NewUserService(hasher *helpers.Md5, tokenManager *helpers.JWT, userRepository repository.UserRepository) *User {
+func NewUserService(hasher *helpers.Md5, tokenManager *helpers.JWT, userRepository repository.UserRepository,
+	authRepository repository.AuthRepository) *User {
 	return &User{
-		hasher:          hasher,
-		tokenManager:    tokenManager,
+		hasher:         hasher,
+		tokenManager:   tokenManager,
 		userRepository: userRepository,
+		authRepository: authRepository,
 	}
+}
+
+func (s *User) CreateUserByAccessToken(ctx context.Context, req *api.CreateUserByAccessTokenRequest) (*api.CreateUserResponseByAccessToken, error) {
+	claims, err := s.tokenManager.ParseJWT(req.AccessToken)
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, "Token incorrect")
+	}
+
+	if claims.ExpiresAt < time.Now().Unix() {
+		return nil, status.Error(codes.Unauthenticated, "Token lifetime expired")
+	}
+
+	if claims.Subject != "root" {
+		return nil, status.Error(codes.PermissionDenied, "Insufficient access rights")
+	}
+
+	if s.authRepository.IsExistByEmail(ctx, req.Login) {
+		return nil, status.Error(codes.AlreadyExists, "Login exists")
+	}
+
+	user := &models.CreateUser{
+		AccessToken: req.AccessToken,
+		Login:       req.Login,
+		Password:    s.hasher.PasswordToMD5Hash(req.Password),
+		Role:        req.Role,
+	}
+
+	err = s.userRepository.CreateUserByAccessToken(ctx, user)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "User creation error")
+	}
+
+	return &api.CreateUserResponseByAccessToken{
+		Message: "User created",
+	}, nil
 }
