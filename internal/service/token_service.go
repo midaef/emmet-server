4aa4ef373792dc1f0abf6b13247c3541f1e43ba2
midaef <danil.present@mail.ru>
2021-04-09 01:01:36 +0800

    create(token service)
    * Add check access token method

---
+++ internal/service/token_service.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"github.com/midaef/emmet-server/pkg/helpers"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"time"
+)
+
+type Token struct {
+	hasher          *helpers.Md5
+	tokenManager    *helpers.JWT
+}
+
+func NewTokenService(hasher *helpers.Md5, tokenManager *helpers.JWT) *Token {
+	return &Token{
+		hasher:          hasher,
+		tokenManager:    tokenManager,
+	}
+}
+
+func (s *Token) CheckAccessToken(accessToken string) (*helpers.Claims, error) {
+	claims, err := s.tokenManager.ParseJWT(accessToken)
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, "Token incorrect")
+	}
+
+	token, err := s.tokenManager.CreateAccessToken(claims)
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, "Token incorrect")
+	}
+
+	if token != accessToken {
+		return nil, status.Error(codes.Unauthenticated, "Token incorrect")
+	}
+
+	if claims.ExpiresAt < time.Now().Unix() {
+		return nil, status.Error(codes.Unauthenticated, "Token lifetime expired")
+	}
+
+	return claims, nil
+}
