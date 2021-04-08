b79398c988137f11422b51a727e24b8c4f49dd89
midaef <danil.present@mail.ru>
2021-04-09 01:09:29 +0800

    update(role service)
    * Add check access token
    * Add check permission
    * Add create role
    * Add get permission

--- internal/service/role_service.go
+++ internal/service/role_service.go
@@ -3,23 +3,59 @@ package service
 import (
 	"context"
 	"github.com/midaef/emmet-server/internal/api"
-
+	"github.com/midaef/emmet-server/internal/models"
+	"github.com/midaef/emmet-server/internal/repository"
 	"github.com/midaef/emmet-server/pkg/helpers"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Role struct {
 	hasher         *helpers.Md5
-	tokenManager   *helpers.JWT
+	tokenService   TokenService
+	roleRepository repository.RoleRepository
 }
 
-func NewRoleService(hasher *helpers.Md5, tokenManager *helpers.JWT) *Role {
+func NewRoleService(hasher *helpers.Md5, tokenService TokenService, roleRepository repository.RoleRepository) *Role {
 	return &Role{
 		hasher:         hasher,
-		tokenManager:   tokenManager,
+		tokenService:   tokenService,
+		roleRepository: roleRepository,
 	}
 }
 
 func (s *Role) CreateRoleByAccessToken(ctx context.Context, req *api.CreateRoleByAccessTokenRequest) (*api.CreateRoleResponseByAccessToken, error) {
+	claims, err := s.tokenService.CheckAccessToken(req.AccessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	permissions, err := s.roleRepository.GetPermissionsByRole(ctx, claims.Subject)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Get permissions error")
+	}
+
+	if !permissions.CreateRole {
+		return nil, status.Error(codes.PermissionDenied, "Insufficient access rights")
+	}
+
+	if s.roleRepository.IsExistByRole(ctx, req.Role) {
+		return nil, status.Error(codes.AlreadyExists, "Role exists")
+	}
+
+	role := &models.Role{
+		CreatedBy:   claims.Subject,
+		CreateRole:  req.CreateRole,
+		CreateUser:  req.CreateUser,
+		CreateValue: req.CreateValue,
+		Role:        req.Role,
+	}
+
+	err = s.roleRepository.CreateRole(ctx, role)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Create role error")
+	}
+
 	return &api.CreateRoleResponseByAccessToken{
 		Message: "Role created",
 	}, nil
